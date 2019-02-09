0d6f07e48096355e447c06699592876ec57421be
MinchinWeb <w_minchin@hotmail.com>
2019-02-09 09:12:40 -0700

    [Tests] updated and expanded!

--- test/test_printing.py
+++ test/test_printing.py
@@ -9,7 +9,21 @@ def test_centered():
     print(wmtext.centered('4. Multi Character Fill', fill='abc'))
     print(wmtext.centered('5. Short Line', 40, '*'))
 
+def test_titles():
+    wmtext.title("Testing Title")
+    wmtext.subtitle("Almost as import text")
+
+def test_rainbow():
+    text = "0123456789"*5
+    for offset in range(7):
+        wmtext.rainbow_print(text, offset)
+
 if __name__ == "__main__":
     import colorama
     colorama.init()
+
+    test_titles()
+    print()
     test_centered()
+    print()
+    test_rainbow()
